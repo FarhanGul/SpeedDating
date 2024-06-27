40e60efb7cfa9e0c98f29afffd81b063c680b50b
FarhanGul <FarhanGulKhan29@gmail.com>
2024-06-27 12:58:34 +0500

    Updated package

--- Assets/_Project/Scripts/Common.lua
+++ Assets/_Project/Scripts/Common.lua
@@ -118,9 +118,9 @@ function CVisibleTopRanks() return 8 end
 function CRelationshipIdDelimiter() return "," end 
 -- Development Constants
 function CUseProductionStorage() return false end
-function CEnableDevCommands() return true end
+function CEnableDevCommands() return false end
 function CEnableUIDebugging() return false end
-function CEnableQuickGame() return true end
+function CEnableQuickGame() return false end
 
 -- Storage Keys
 function KDatingLeaderboard() return CUseProductionStorage() and "DatingLeaderboard" or "_DatingLeaderboard" end
